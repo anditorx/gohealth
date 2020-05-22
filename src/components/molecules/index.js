2424db336c0d83b3dc30327d83b0b4d023d8d876
andiankas <rustiantoandi17@gmail.com>
2020-05-22 21:16:46 +0700

    prepare base component

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,4 +1,15 @@
 import Header from './Header';
 import ButtomNavigator from './ButtomNavigator';
+import HomeProfile from './HomeProfile';
+import DoctorCategory from './DoctorCategory';
+import RatedDoctor from './RatedDoctor';
+import NewsItem from './NewsItem';
 
-export {Header, ButtomNavigator};
+export {
+  Header,
+  ButtomNavigator,
+  HomeProfile,
+  DoctorCategory,
+  RatedDoctor,
+  NewsItem,
+};
